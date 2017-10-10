693aacdd4d2f87c505cea39e30fffc2ef21e6df9
Nick Grismore <nicksk83@aol.com>
2017-10-10 14:47:59 -0400

    added BasicCard constructor

--- BasicCard.js
+++ BasicCard.js
@@ -1,46 +1,11 @@
-var inquirer = require('inquirer');
-
-inquirer.prompt([
-  {
-    type: 'list',
-    name: 'theme',
-    message: 'What do you want to do?',
-    choices: [
-      'Order a pizza',
-      'Make a reservation',
-      new inquirer.Separator(),
-      'Ask for opening hours',
-      {
-        name: 'Contact support',
-        disabled: 'Unavailable at this time'
-      },
-      'Talk to the receptionist'
-    ]
-  },
-  {
-    type: 'list',
-    name: 'size',
-    message: 'What size do you need?',
-    choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
-    filter: function (val) {
-      return val.toLowerCase();
-    }
-  }
-]).then(function (answers) {
-  console.log(JSON.stringify(answers, null, '  '));
-});
-
-var cards = function(front,back){
+function BasicCard(front,back){
 	this.front = front;
 	this.back = back;
-
-	this.frontCard = function(front){
-		console.log();
-	}
-	this.backCard = function (back){
-		console.log();
-	}
 }
+
+var firstCard =  new BasicCard("question","answer");
+
+console.log(firstCard.front,firstCard.back);
 
 
 
