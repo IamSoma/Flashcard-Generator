b946626c72fb8b2804dbed856f82984b02349ebc
Nick Grismore <nicksk83@aol.com>
2017-10-09 22:29:49 -0400

    added test code

--- BasicCard.js
+++ BasicCard.js
@@ -1,14 +1,44 @@
 var inquirer = require('inquirer');
 
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'theme',
+    message: 'What do you want to do?',
+    choices: [
+      'Order a pizza',
+      'Make a reservation',
+      new inquirer.Separator(),
+      'Ask for opening hours',
+      {
+        name: 'Contact support',
+        disabled: 'Unavailable at this time'
+      },
+      'Talk to the receptionist'
+    ]
+  },
+  {
+    type: 'list',
+    name: 'size',
+    message: 'What size do you need?',
+    choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+    filter: function (val) {
+      return val.toLowerCase();
+    }
+  }
+]).then(function (answers) {
+  console.log(JSON.stringify(answers, null, '  '));
+});
+
 var cards = function(front,back){
 	this.front = front;
 	this.back = back;
 
 	this.frontCard = function(front){
-
+		console.log();
 	}
 	this.backCard = function (back){
-
+		console.log();
 	}
 }
 
